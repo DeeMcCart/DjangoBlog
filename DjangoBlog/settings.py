45778cd2581bd2433503d5cb8341b01e9e81b5e0
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-10-06 07:27:16 +0000

    Add post detail views

--- DjangoBlog/settings.py
+++ DjangoBlog/settings.py
@@ -17,7 +17,7 @@ if os.path.isfile("env.py"):
     import env
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-TEMPLATES_DIR=os.path.join(BASE_DIR, 'templates')
+TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
 
 # Quick-start development settings - unsuitable for production
@@ -117,6 +117,7 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+ACCOUNT_EMAIL_VERIFICATION = 'none'
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
