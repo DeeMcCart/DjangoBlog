e11956f009c2c7b52527c841fb6db88d8e064316
Deirdre McCarthy <deirdremc1968@gmail.com>
2023-10-17 16:47:32 +0000

    Add like submission for authenticated users

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, reverse
 from django.views import generic, View
 from .models import Post
 from .forms import CommentForm
@@ -12,7 +12,6 @@ class PostList(generic.ListView):
 
 
 class PostDetail(View):
-    
     def get(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
@@ -63,3 +62,16 @@ class PostDetail(View):
                 "liked": liked,          
             },
         )
+
+
+class PostLike(View):
+    
+    def post(self, request, slug):
+
+        post = get_object_or_404(Post, slug=slug)
+        if post.likes.filter(id=request.user.id).exists():
+            post.likes.remove(request.user)
+        else:
+            post.likes.add(request.user)
+        
+        return HttpResponseRedirect(reverse('post_detail', args=[slug]))
